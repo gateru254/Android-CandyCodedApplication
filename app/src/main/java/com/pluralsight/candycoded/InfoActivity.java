cc031df6e795d79b3a15d756ac888e43324ba3f5
kelvin <kelvinkiarie6@gmail.com>
2020-10-09 22:56:08 +0300

    changing the uri

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,13 +1,15 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-public class InfoActivity extends AppCompatActivity {
+public class    InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,15 @@ public class InfoActivity extends AppCompatActivity {
                 into(candyStoreImageView);
 
 
+
+    }
+    public void createMapIntent(View view){
+        Uri uri= Uri.parse("geo:0,0?q=618 E South St Orlando, FL32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager())!=null){
+            startActivity(mapIntent);
+        }
     }
 
     // ***
